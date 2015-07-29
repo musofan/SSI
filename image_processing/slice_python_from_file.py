a892705473f42ed109f05d0f4b6d9f16418627df
damoncrockett <damoncrockett@hotmail.com>
2015-07-28 22:46:53 -0700

    file slicer
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

---
+++ image_processing/slice_python_from_file.py
@@ -0,0 +1,24 @@
+import image_slicer
+import pandas as pd
+import os
+import sys
+
+infile = sys.argv[1]
+num_slices = sys.argv[2]
+target = sys.argv[3]
+
+df = pd.read_csv(infile)
+
+# option to sample
+df = df.sample(n=360)
+df.reset_index(drop=True,inplace=True)
+
+n = len(df.index)
+for i in range(n):
+    try:
+        file = df.filename.loc[i]
+        tiles = image_slicer.slice(file,num_slices,save=False)
+        image_slicer.save_tiles(tiles,directory=target,prefix=os.path.basename(file))
+        print i
+    except:
+        print 'err'
